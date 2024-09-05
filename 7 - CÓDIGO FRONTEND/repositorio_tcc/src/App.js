aaf335fe1d513b81ab78031035d4b803c3629afd
Everson <everson.mimartinez@gmail.com>
2024-09-05 01:01:21 -0300

    feat: Tela de login

--- 7 - CÓDIGO FRONTEND/repositorio_tcc/src/App.js
+++ 7 - CÓDIGO FRONTEND/repositorio_tcc/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/Login.js'
 
 class App extends React.Component{
   
   render(){
     return (
-      <div> 
-        <h1>Olá, Femass!</h1>
-      </div>
+      <BrowserRouter>
+        <div className="container-fluid">
+          <Routes>
+            <Route exact path="/" element={<Login />}></Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ 7 - CÓDIGO FRONTEND/repositorio_tcc/src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import logoFemass from './../assets/images/logo-femass.png';
+import logo from './../assets/images/Logo TCCFLOW.png';
+import './login.css';
+export default class Login extends Component {
+  render() {
+    return (
+      <div className='container-fluid d-flex flex-column justify-content-between' style={{'min-height': '100vh'}}>
+        <div className='row justify-content-center' style={{'margin-top': '10vh'}}> 
+          <div className='col-10 col-md-6 col-lg-4 text-center'>
+            <img src={logoFemass} className="img-fluid" style={{'max-width': '300px'}} alt="Logo da faculdade"></img>
+          </div>
+        </div>  
+        <div className='row mt-5 justify-content-center'>
+          <div className='col-10 col-md-6 col-lg-3'>
+            <div className='bg-white border rounded p-4'>
+              <div className="mb-3 row justify-content-center">  
+                <div className='col-12'>
+                  <label for="loginUser">Login</label>
+                  <input type="email" className="form-control" id="loginUser" placeholder="Matrícula"></input>
+                </div>
+              </div>
+              <div className="mb-4 row justify-content-center">
+                <div className='col-12'>
+                  <label for="loginPassword" class="col-form-label">Senha</label>
+                  <input type="password" class="form-control" id="loginPassword"></input>
+                </div>
+              </div>
+              <div className='mb-5 row justify-content-center'>
+                <div className='col-12 d-grid'>
+                  <button className='btn btn-custom'>Entrar no sistema</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div> 
+        <div className='row justify-content-end mt-auto'>
+          <div className='col-6 col-md-4 text-end'>
+            <img src={logo} className="img-fluid mb-3 me-2" style={{'max-width': '180px'}} alt="Logo da faculdade"></img>
+          </div> 
+        </div>
+      </div>
+    )
+  }
+}
