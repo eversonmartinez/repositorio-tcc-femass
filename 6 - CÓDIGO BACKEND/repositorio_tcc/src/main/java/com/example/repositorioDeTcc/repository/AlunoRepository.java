d872006f14d62a16db7bd595b5a20ddca93bcc81
Everson Martinez <everson.mimartinez@gmail.com>
2024-09-28 01:02:38 -0300

    feat: repository para aluno, orientador e professor

---
+++ 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/repository/AlunoRepository.java
@@ -0,0 +1,11 @@
+package com.example.repositorioDeTcc.repository;
+
+import com.example.repositorioDeTcc.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
+    public List<Aluno> findAllByAtivoIsTrue();
+}
