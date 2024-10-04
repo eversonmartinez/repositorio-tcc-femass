98b95702d3303e726e61ee47307ad5b088b85e7d
Everson Martinez <everson.mimartinez@gmail.com>
2024-10-04 20:30:30 -0300

    feat: migatrion e controller para tcc

---
+++ 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/controller/TCCController.java
@@ -0,0 +1,54 @@
+package com.example.repositorioDeTcc.controller;
+
+import com.example.repositorioDeTcc.dto.TCCDTO;
+import com.example.repositorioDeTcc.dto.TCCMinDTO;
+import com.example.repositorioDeTcc.service.TCCService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/tcc")
+public class TCCController {
+
+    @Autowired
+    TCCService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<TCCDTO> findById(@PathVariable UUID id){
+        TCCDTO result = service.findById(id);
+        return ResponseEntity.ok().body(result);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TCCMinDTO>> findAll(){
+        List<TCCMinDTO> result = service.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TCCDTO> create(@RequestBody TCCDTO tcc){
+
+        tcc = service.insert(tcc);
+        return ResponseEntity.ok().body(tcc);
+    }
+
+    @PutMapping(value = "/{id}" , produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<TCCDTO> update(@PathVariable UUID id, @RequestBody TCCDTO tcc){
+
+        tcc = service.update(id, tcc);
+        return ResponseEntity.ok().body(tcc);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id")UUID id){
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
+    }
+}
