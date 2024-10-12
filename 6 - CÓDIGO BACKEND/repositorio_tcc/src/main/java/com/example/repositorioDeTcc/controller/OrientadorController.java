5d9bd040457203e7025afc916d4b2c07e358de7c
Davi Trajano <90059259+davi-tr@users.noreply.github.com>
2024-10-12 12:34:57 -0300

    feat: mudar a senha no primeiro login

--- 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/controller/OrientadorController.java
+++ 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/controller/OrientadorController.java
@@ -6,6 +6,7 @@ import com.example.repositorioDeTcc.service.OrientadorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,6 +15,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping(value = "/orientadores")
 @CrossOrigin
+@PreAuthorize("hasAnyRole('USER','ADMIN','MODERATOR')")
 public class OrientadorController {
 
     @Autowired
