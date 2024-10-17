e2ceec3fb6442f3113e492d8595c400d48524c70
Everson Martinez <everson.mimartinez@gmail.com>
2024-10-16 23:06:10 -0300

    feat: Modal para troca de senha e uso de context api

--- 7 - CÓDIGO FRONTEND/repositorio_tcc/src/App.js
+++ 7 - CÓDIGO FRONTEND/repositorio_tcc/src/App.js
@@ -8,6 +8,8 @@ import FirstScreen from './components/firstScreen/FirstScreen.js';
 import ProtectedRoute from './HOC/ProtectedRoute';
 import Orientador from './components/orientador/Orientador.js';
 import TCC from './components/tcc/TCC.js';
+import { PasswordModalProvider } from './components/passwordChange/PasswordModalContext'; // O provedor do contexto
+import PasswordChangeModal from './components/passwordChange/PasswordChangeModal'; // O modal global
 
 class App extends React.Component{
   
@@ -15,14 +17,17 @@ class App extends React.Component{
     return (
       <BrowserRouter>
         <div className="container-fluid">
+        <PasswordModalProvider>
           <Routes>
-            <Route exact path="/" element={<FirstScreen />}></Route>
-            <Route exact path="/home" element={<ProtectedRoute component={Home} />}></Route>
-            <Route exact path="/login" element={<Login />}></Route>
-            <Route exact path="/alunos" element={<ProtectedRoute component={Aluno} />}></Route>
-            <Route exact path="/orientadores" element={<ProtectedRoute component={Orientador} />}></Route>
-            <Route exact path="/tcc" element={<ProtectedRoute component={TCC} />}></Route>
+              <Route exact path="/" element={<FirstScreen />}></Route>
+              <Route exact path="/home" element={<ProtectedRoute component={Home} />}></Route>
+              <Route exact path="/login" element={<Login />}></Route>
+              <Route exact path="/alunos" element={<ProtectedRoute component={Aluno} />}></Route>
+              <Route exact path="/orientadores" element={<ProtectedRoute component={Orientador} />}></Route>
+              <Route exact path="/tcc" element={<ProtectedRoute component={TCC} />}></Route>
           </Routes>
+          <PasswordChangeModal />
+        </PasswordModalProvider>
         </div>
       </BrowserRouter>
     );
