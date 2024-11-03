994dd68a18e71abb54164dd4b4380fc67d53852c
Guilherme Maia <xguilherme.ca.maiax@gmail.com>
2024-11-02 22:58:36 -0300

    Commit da tela de cadastro de orientador completa

--- 7 - CÓDIGO FRONTEND/repositorio_tcc/src/components/orientador/CadastroOrientador.js
+++ 7 - CÓDIGO FRONTEND/repositorio_tcc/src/components/orientador/CadastroOrientador.js
@@ -4,7 +4,8 @@ import logo from './../../assets/images/Logo TCCFLOW.png';
 import { useNavigate } from 'react-router-dom';
 import InputMask from 'react-input-mask';
 import { ToastContainer, toast } from 'react-toastify';
-import DataTable from 'react-data-table-component';
+import { Button, Modal } from 'react-bootstrap';
+
 
 function withNavigate(Component) {
     return (props) => {
@@ -16,17 +17,14 @@ function withNavigate(Component) {
 class cadastroOrientador extends Component {
     
     resgister = (event) => {
-        console.log("botão apertado");
         event.preventDefault();
-        var url = window.server + "/orientadores";
 
-        let cpf = this.state.cpf.replace(/[^\d]/g, '')
-        console.log(cpf)
+        var url = window.server + "/orientadores";
 
         const data = {
             "nomeCompleto": this.state.nomeCompleto,
             "cpf": this.state.cpf.replace(/[^\d]/g, ''),
-            "telefone": this.state.cpf.replace(/[^\d]/g, ''),
+            "telefone": this.state.telefone.replace(/[^\d]/g, ''),
             "email": this.state.email
         };
         const token = sessionStorage.getItem('token');
@@ -50,7 +48,9 @@ class cadastroOrientador extends Component {
                         pauseOnHover: true,
                         draggable: true,
                         progress: undefined,
+                        
                     });
+                    this.clearState()
                     return;
                 } else {
                     toast.error('Erro ao criar', {
@@ -66,44 +66,21 @@ class cadastroOrientador extends Component {
                 }
             })
             .catch(e => { console.error(e) })
-        
         setTimeout(() => {
             this.fillList();
-        }, 5000);
+        }, 2000);
     }
-
-    txtNomeCompleto_change = (event) => {
-        this.setState({ nomeCompleto: event.target.value });
-    }
-
-    txtCPF = (event) => {
-        this.setState({ cpf: event.target.value });
-    }
-
-    txtTelefone = (event) => {
-        this.setState({ telefone: event.target.value });
-    }
-
-    txtEmail = (event) => {
-        this.setState({ email: event.target.value });
-    }
-
-    // carregar os dados
 
     state = {
         listOrientador: [],
-        showModalDeletion: false
+        showModalDeletion: false,
+        nomeCompleto: '',
+        cpf: '',
+        telefone: '',
+        email:''
     }
 
-    // columns = [
-    //     {
-    //         name: "Ações",
-    //         cell: listOrientador => <>
-    //             <button class="btn btn-warning">editar</button>
-    //             <button class="btn btn-danger" onClick={this.beginDeletion(listOrientador)} >Deletar</button>
-    //         </>, width: '12%'
-    //     }
-    // ];
+    // carregar os dados na lista
 
     fillList = () => {
         const url = window.server + "/orientadores";
@@ -122,7 +99,6 @@ class cadastroOrientador extends Component {
             .then((response) => response.json())
             .then((data) => this.setState({ listOrientador: data, filteredItems: data }));
         
-        //console.log(this.state.listOrientador);
     }
 
     beginView = () => {
@@ -159,17 +135,11 @@ class cadastroOrientador extends Component {
     // Deletar orientador
 
     beginDeletion = (listOrientador) => {
-        //console.log(listOrientador.nomeCompleto)
         this.setState({ toDeleteItem: listOrientador, showModalDeletion: true });
-        console.log(this.state.toDeleteItem.id)
     }
 
-    delete = (listOrientador) => {
-        console.log(listOrientador.id)
-        this.setState({ toDeleteItem: listOrientador, showModalDeletion: true });
-        console.log(this.state.toDeleteItem)
-
-        const url = window.server + "/orientadores/" + this.setState.toDeleteItem.id;
+    delete = () => {
+        const url = window.server + "/orientadores/" + this.state.toDeleteItem.id;
 
         const token = sessionStorage.getItem('token');
 
@@ -211,6 +181,144 @@ class cadastroOrientador extends Component {
             });
     }
 
+    // Edição
+    beginEdit = (listOrientador) => {
+
+        const url = window.server + "/orientadores/" + listOrientador.id;
+
+        const token = sessionStorage.getItem('token');
+
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token, // Adicione o token JWT
+                'Content-Type': 'application/json'
+            }
+        };
+
+        fetch(url, requestOptions)
+            .then((response) => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Erro na requisição: ' + response.status);
+                }
+            }).then((data) => {
+                if (data.nomeCompleto === null) data.nomeCompleto = '';
+                this.setState({
+                    toEditItem: data,
+                    showModalEdit: true,
+                    nomeCompleto: data.nomeCompleto,
+                    cpf:data.cpf,
+                    telefone:data.telefone,
+                    email:data.email
+                });
+            })
+            .catch((error) => {
+            });
+    }
+
+    submitOrientadorForm = (event) => {
+        event.preventDefault();
+
+        let url = window.server + "/orientadores";
+
+        const token = sessionStorage.getItem('token');
+
+        let data = {
+            "nomeCompleto": this.state.nomeCompleto,
+            "cpf": this.state.cpf.replace(/[^\d]/g, ''),
+            "telefone": this.state.telefone.replace(/[^\d]/g, ''),
+            "email": this.state.email
+        }
+
+        if (this.state.resumo) {
+            data.resumo = this.state.resumo;
+        }
+
+        let requestOptions = {
+            method: 'POST',
+            headers: {
+                'Authorization': 'Bearer ' + token, // Adicione o token JWT
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        };
+
+        if (this.state.toEditItem) {
+            requestOptions.method = 'PUT';
+            url += "/" + this.state.toEditItem.id;
+        }
+
+        fetch(url, requestOptions)
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                } else {
+                    throw new Error('Erro na requisição: ' + response.status);
+                }
+            })
+            .then(data => {
+                if (this.state.toEditItem) {
+                    this.setState({ showModalEdit: false })
+                    toast.success('Orientador atualizado!', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
+                } else {
+                    document.getElementById('btnCloseModal').click();
+                    toast.success('TCC criado!', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                    });
+                }
+                this.clearState();
+                this.fillList();
+                // Lógica para lidar com a resposta da API
+            })
+            .catch(error => {
+                toast.error('Ocorreu um erro', {
+                    position: "top-right",
+                    autoClose: 3000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                });
+                // Lógica para lidar com o erro
+            });
+    }
+
+    closeModal = (operationName) => {
+        this.clearState();
+        this.setState({ ['showModal' + operationName]: false, ['to' + operationName + 'Item']: null });
+    }
+
+    handleChange = (event) => {
+        if (event.target.name.startsWith('filter')) this.applyFilters();
+        this.setState({ [event.target.name]: event.target.value });
+    };
+
+    clearState = () => {
+        this.setState({
+            nomeCompleto: "",
+            cpf: "",
+            telefone: "",
+            email: ""
+        });
+    }
+
 
     render() {
         return (
@@ -229,26 +337,25 @@ class cadastroOrientador extends Component {
                                     <div><h3 >Cadastro de Orientador</h3></div>
                                     <div class="mb-3 justify-content-center">
                                         <label for="exampleInputEmail1" class="form-label">Nome Completo</label>
-                                        <input type="text" class="form-control" id="NomeOrientador" onChange={this.txtNomeCompleto_change} />
+                                        <input type="text" class="form-control" name='nomeCompleto' id="nomeCompleto" onChange={this.handleChange} value={this.state.nomeCompleto} required />
                                     </div>
                                     <div class="mb-3 justify-content-center">
                                         <label for="InputCPF" class="form-label">CPF</label>
                                         <InputMask mask="999.999.999-99"
-                                            type="text" class="form-control" id="CPFOrientador" onChange={this.txtCPF} >
+                                            type="text" class="form-control" name='cpf' id="cpf" onChange={this.handleChange} value={this.state.cpf} required >
                                             {(inputProps) => <input {...inputProps} type="text" />}
                                         </InputMask>
-                                        {/* <input type="text" class="form-control" id="CPFOrientador" onChange={this.txtCPF} /> */}
                                     </div>
                                     <div class="mb-3 justify-content-center">
                                         <label for="InputTelefone" class="form-label">telefone</label>
                                         <InputMask mask="(99)9999-99999"
-                                            type="text" class="form-control" id="TelefoneOrientador" onChange={this.txtTelefone} >
+                                            type="text" class="form-control" name='telefone' id="telefone" onChange={this.handleChange} value={this.state.telefone} required >
                                             {(inputProps) => <input {...inputProps} type="text" />}
                                         </InputMask>
                                     </div>
                                     <div class="mb-3 justify-content-center">
                                         <label for="exampleInputEmail1" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="NomeOrientador" required placeholder="email@email.com" onChange={this.txtEmail} />
+                                        <input type="email" class="form-control" name='email' id="email" required placeholder="email@email.com" onChange={this.handleChange} value={this.state.email} />
                                     </div>
                                     <div class="mb-3 justify-content-center">
                                         <button class="btn btn-dark" type='submit'>Criar</button>
@@ -269,26 +376,82 @@ class cadastroOrientador extends Component {
                                 </tr>
                                 {this.state.listOrientador && this.state.listOrientador.length > 0 ? this.state.listOrientador.map(data => (
                                     <tr key={this.state.listOrientador.id}>
-                                        {/* <th scope="row">{data.id}</th> */}
                                         <td className="text-center">{data.nomeCompleto}</td>
                                         <td className="text-center">{data.email}</td>
-                                        <button class="btn btn-danger" onClick={() => this.delete(data)} >Deletar</button>
-                                        {/* <DataTable>
-                                            columns={this.columns}
-                                        </DataTable> */}
+                                        <button class="btn btn-danger" onClick={() => this.beginDeletion(data)} >Deletar</button>
+                                        <button class="btn btn-warning" onClick={() => this.beginEdit(data)}>editar</button>
                                     </tr>
                                 )) : <tr><td colSpan={4} className='text-center fw-bold'>Nenhum orientador encontrado</td></tr>}
                             </tbody>
                         </table>
-                        <button class="btn btn-warning">editar</button>
-                        <button class="btn btn-danger" >Deletar</button>
                     </div>
                     <div className='row justify-content-end mt-auto'>
                     <div className='col-6 col-md-4 text-end'>
                         <img src={logo} className="img-fluid mb-3 me-2" style={{ 'maxWidth': '180px' }} alt="Logo da faculdade"></img>
                     </div>
                     </div>
-                    
+
+                    {/* Modal de exclusão */}
+
+                    <Modal show={this.state.showModalDeletion} onHide={() => this.setState({ toDeleteItem: null, showModalDeletion: false })} centered>
+                        <Modal.Header className='bg-dark text-white' closeButton>
+                            <Modal.Title>Confirmar Exclusão</Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>Tem certeza que deseja excluir o Orientador {this.state.toDeleteItem && <span className='fw-bold'>{this.state.toDeleteItem.nomeCompleto}</span>}?</Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={() => this.setState({ toDeleteItem: null, showModalDeletion: false })}>
+                                Cancelar
+                            </Button>
+                            <Button variant="primary" onClick={this.delete}>
+                                Confirmar
+                            </Button>
+                        </Modal.Footer>
+                    </Modal>
+
+                    {/* Modal de Edição */}
+
+                    <Modal show={this.state.showModalEdit} onHide={() => this.closeModal('Edit')} centered size='xl'>
+                        <Modal.Header className='bg-dark text-white' closeButton>
+                            <Modal.Title>Editar {this.state.toEditItem && this.state.toEditItem.titulo}</Modal.Title>
+                        </Modal.Header>
+                        <form onSubmit={this.submitOrientadorForm}>
+                            <Modal.Body>
+                                {this.state.toEditItem && <>
+                                    <div className="modal-body">
+                                        <div className="container">
+                                            <div className="mb-3 row">
+                                                <div className="col-12">
+                                                    <label htmlFor="inputName" className="col-4 col-form-label fw-bold required">Nome Completo</label>
+                                                    <input type="text" className="form-control" name="nomeCompleto" id="nomeCompleto" onChange={this.handleChange} value={this.state.nomeCompleto} required />
+                                                </div>
+                                                <div className="col-12">
+                                                    <label htmlFor="inputName" className="col-12 col-form-label fw-bold">CPF</label>
+                                                    <InputMask mask="999.999.999-99"
+                                                        type="text" class="form-control" name="cpf" id="cpf" onChange={this.handleChange} value={this.state.cpf} />
+                                                </div>
+                                                <div className="col-12">
+                                                    <label htmlFor="inputName" className="col-12 col-form-label fw-bold">Telefone</label>
+                                                    <InputMask mask="(99)9999-99999"
+                                                        type="text" class="form-control" name="telefone" id="telefone" onChange={this.handleChange} value={this.state.telefone} />
+                                                </div>
+                                                <div className="col-12">
+                                                    <label htmlFor="inputName" className="col-12 col-form-label fw-bold">Email</label>
+                                                    <input type="text" className="form-control" name="email" id="email" onChange={this.handleChange} value={this.state.email}/>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </>
+                                }
+                            </Modal.Body>
+                            <Modal.Footer>
+                                <Button variant="secondary" onClick={() => this.closeModal('Edit')}>
+                                    Fechar
+                                </Button>
+                                <button type='submit' className="btn btn-primary">Salvar</button>
+                            </Modal.Footer>
+                        </form>
+                    </Modal>
                 </div>
             </div>
         )
