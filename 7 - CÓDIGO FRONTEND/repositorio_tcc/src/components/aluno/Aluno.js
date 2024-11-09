9017df719693f0f456d43c31bc26f904350925e7
Uriel Guimarães <urielguima@gmail.com>
2024-11-09 19:37:09 -0300

    Página de Aluno, versão completa

--- 7 - CÓDIGO FRONTEND/repositorio_tcc/src/components/aluno/Aluno.js
+++ 7 - CÓDIGO FRONTEND/repositorio_tcc/src/components/aluno/Aluno.js
@@ -17,6 +17,17 @@ class Aluno extends Component {
         password: '',
         passwordConfirm: '',
         filterText: '',
+        toDeleteItem: null,
+        toViewItem: null,
+        toEditItem: null,
+        showModalDeletion: false,
+        showModalEdit: false,
+        showModalRegistration: false,
+        showModalView: false,
+        nomeCompleto: null,
+        email: null,
+        telefone: null,
+        matricula: null,
     }
 
     columns = [
@@ -47,9 +58,9 @@ class Aluno extends Component {
         {
             name: 'Ações',
             cell: user => <>
-                <button className="btn btn-outline-secondary mx-1 px-1 py-0" data-toggle="tooltip" data-placement="top" title="Visualizar Instituto"><i className="bi bi-eye"></i></button>
-                <button className="btn btn-outline-secondary mx-1 px-1 py-0" data-toggle="tooltip" data-placement="top" title="Editar Instituto"><i className="bi bi-pencil"></i></button>
-                <button className="btn btn-outline-secondary mx-1 px-1 py-0" data-toggle="tooltip" data-placement="top" title="Excluir selecionado"><i className="bi bi-trash"></i></button>
+                <button className="btn btn-outline-secondary mx-1 px-1 py-0" data-toggle="tooltip" data-placement="top" title="Visualizar Instituto" onClick={() => this.beginView(user)}><i className="bi bi-eye"></i></button>
+                <button className="btn btn-outline-secondary mx-1 px-1 py-0" data-toggle="tooltip" data-placement="top" title="Editar Instituto" onClick={() => this.beginEdit(user)}><i className="bi bi-pencil"></i></button>
+                <button className="btn btn-outline-secondary mx-1 px-1 py-0" data-toggle="tooltip" data-placement="top" title="Excluir selecionado" onClick={() => this.beginDeletion(user)}><i className="bi bi-trash"></i></button>
             </>,
              width: '20%'
         }
@@ -84,18 +95,22 @@ class Aluno extends Component {
     }
 
     handleChange = (event) => {
-        //definição de funções callbacks, caso necessárias
-        let callback = null;
-        //se o evento for de checkbox, as alterações prosseguirão a partir de outra função
-        if(event.target.name.startsWith('checkbox')) {
-            this.handleCheckboxChange(event);
-            return;
-        }
-        if(event.target.name.startsWith('password')) callback = this.validatePassword;
-        if(event.target.name === 'filterText') callback = this.applyFilters;
-
-        this.setState({ [event.target.name]: event.target.value }, callback);
+        if(event.target.name.startsWith('filter')) this.applyFilters();
+        this.setState({ [event.target.name]: event.target.value });
     };
+    // handleChange = (event) => {
+    //     //definição de funções callbacks, caso necessárias
+    //     let callback = null;
+    //     //se o evento for de checkbox, as alterações prosseguirão a partir de outra função
+    //     if(event.target.name.startsWith('checkbox')) {
+    //         this.handleCheckboxChange(event);
+    //         return;
+    //     }
+    //     if(event.target.name.startsWith('password')) callback = this.validatePassword;
+    //     if(event.target.name === 'filterText') callback = this.applyFilters;
+
+    //     this.setState({ [event.target.name]: event.target.value }, callback);
+    // };
 
     fillList = () => {
         const url = window.server + "/alunos";
@@ -124,6 +139,47 @@ class Aluno extends Component {
         this.setState({ showModalRegistration: true });
     }
 
+    beginEdit = (aluno) => { 
+
+        const url = window.server + "/alunos/" + aluno.id;
+
+        const token = sessionStorage.getItem('token');
+
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token, // Adicione o token JWT
+                'Content-Type': 'application/json'
+            }
+        };
+
+        fetch(url,requestOptions)
+            .then((response) => {
+                if(response.ok) {
+                    return response.json();
+                } else{
+                    throw new Error('Erro na requisição: ' + response.status);
+                }
+            }).then((data) => { 
+                if (data.resumo === null) data.resumo = '';
+                this.setState({ 
+                    toEditItem: data,
+                    showModalEdit: true,
+                    nomeCompleto: data.nomeCompleto,
+                    email: data.email,
+                    telefone: data.telefone,
+                    // tituloTcc: data.titulo,
+                    // resumo: data.resumo,
+                    // // selectedCurso: { value: data.idCurso, label: data.idCurso },
+                    // // selectedAluno: { value: data.idAluno, label: data.nomeCompletoAluno },
+                    // // selectedOrientador: { value: data.idOrientador, label: data.nomeCompletoOrientador } 
+                
+                });
+            })
+            .catch((error) => {
+            });
+    }
+
     validatePassword = () => {
         if(!this.state.password || !this.state.passwordConfirm) return false;
         const passwordConfirmElement = document.getElementById('passwordConfirm');
@@ -149,13 +205,20 @@ class Aluno extends Component {
 
     clearState = () => {
         this.setState({
-            completeName: '',
-            login: '',
-            email: '',
-            password: '',
-            passwordConfirm: '',
-            checkboxChangePassword: true,
-            checkboxNotifyEmail: true
+            nomeCompleto: "",
+            email: "",
+            telefone: "",
+            matricula: "",
+            toDeleteItem: null,
+            toViewItem: null,
+            toEditItem: null,
+            // completeName: '',
+            // login: '',
+            // email: '',
+            // password: '',
+            // passwordConfirm: '',
+            // checkboxChangePassword: true,
+            // checkboxNotifyEmail: true
         });
     }
 
@@ -165,6 +228,7 @@ class Aluno extends Component {
     }
 
     registerForm = (event) => {
+
         event.preventDefault();
         
         if(!this.validateForm()) {
@@ -182,8 +246,8 @@ class Aluno extends Component {
 
         let url = window.server + "/alunos";
 
-        const data = {
-            "nomeCompleto": this.state.completeName,
+        let data = {
+            "nomeCompleto": this.state.nomeCompleto,
             "matricula": this.state.matricula,
             "email": this.state.email,
             "telefone": this.state.telefone,
@@ -200,10 +264,33 @@ class Aluno extends Component {
             body: JSON.stringify(data)
         };
 
+        if(this.state.toEditItem){
+            requestOptions.method = 'PUT';
+            url+= "/" + this.state.toEditItem.id;
+        }
+
         fetch(url, requestOptions)
-        .then((response) => {
-            if (response.status === 200) {
-                this.closeModal('Registration');
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error('Erro na requisição: ' + response.status);
+            }
+        })
+        .then(data => {
+            if(this.state.toEditItem){
+                this.setState({showModalEdit: false})
+                toast.success('Usuário atualizado!', {
+                    position: "top-right",
+                    autoClose: 2000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                  });
+            } else{
+                this.closeModal('Registration')
                 toast.success('Usuário criado!', {
                     position: "top-right",
                     autoClose: 2000,
@@ -213,25 +300,104 @@ class Aluno extends Component {
                     draggable: true,
                     progress: undefined,
                 });
-                setTimeout(() => {
-                }, 2000);
-                this.clearState();
-                this.fillList();
-                return;
-            } else {
-            toast.error('Erro ao criar', {
+            }
+            this.clearState();
+            this.fillList();
+            // Lógica para lidar com a resposta da API
+        })
+        .catch(error => {
+            console.log(error)
+            toast.error('Ocorreu um erro', {
                 position: "top-right",
-                autoClose: 2000,
+                autoClose: 3000,
                 hideProgressBar: false,
                 closeOnClick: true,
                 pauseOnHover: true,
                 draggable: true,
                 progress: undefined,
-            });
-            throw new Error('Falha na requisição: ' + response.status);
+              });
+            // Lógica para lidar com o erro
+            this.clearState();
+        });
+    }
+    
+    delete = () => {
+
+        console.log(this.state.toDeleteItem)
+        
+        const url = window.server + "/alunos/" + this.state.toDeleteItem.id;
+
+        const token = sessionStorage.getItem('token');
+
+        const requestOptions = {
+            method: 'DELETE',
+            headers: {
+                'Authorization': 'Bearer ' + token, // Adicione o token JWT
+                'Content-Type': 'application/json'
             }
-        })
-        .catch(e => { console.error(e) });
+        };
+
+        fetch(url,requestOptions)
+            .then((response) => {
+                if(response.ok) {
+                    this.fillList();
+                    this.setState({ showModalDeletion: false });
+                    toast.success('Aluno excluído!', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                      });
+                }
+                else{
+                    toast.error('Não foi possível excluir', {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        pauseOnHover: true,
+                        draggable: true,
+                        progress: undefined,
+                      });
+                    throw new Error('Erro na requisição: ' + response.status);
+                }
+            });
+    }
+
+    beginDeletion = (alunos) => {
+        this.setState({ toDeleteItem: alunos, showModalDeletion: true });
+        console.log(this.state.showModalDeletion)
+	}
+
+    beginView = (aluno) => { 
+
+        const url = window.server + "/alunos/" + aluno.id;
+
+        const token = sessionStorage.getItem('token');
+
+        const requestOptions = {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token, // Adicione o token JWT
+                'Content-Type': 'application/json'
+            }
+        };
+
+        fetch(url,requestOptions)
+            .then((response) => {
+                if(response.ok) {
+                    return response.json();
+                } else{
+                    throw new Error('Erro na requisição: ' + response.status);
+                }
+            }).then((data) => {
+                this.setState({ toViewItem: data, showModalView: true });
+            })
+            .catch((error) => {
+            });
     }
 
     componentDidMount() {
@@ -295,14 +461,14 @@ class Aluno extends Component {
             <div className='modals'>
                 <Modal show={this.state.showModalRegistration} onHide={() => this.closeModal('Registration')} centered>
                     <Modal.Header closeButton className='bg-dark text-white'>
-                        <Modal.Title>Novo Usuário</Modal.Title>
+                        <Modal.Title>Novo Aluno</Modal.Title>
                     </Modal.Header>
                     <form onSubmit={this.registerForm}>
                     <Modal.Body>
                             <div className="mb-3 row justify-content-center">  
                                 <div className='col-12'>
-                                <label htmlFor="completeName" className='required'>Nome Completo</label>
-                                <input type="text" className="form-control" id="completeName" name="completeName" required onChange={this.handleChange}></input>
+                                <label htmlFor="nomeCompleto" className='required'>Nome Completo</label>
+                                <input type="text" className="form-control" id="nomeCompleto" name="nomeCompleto" required onChange={this.handleChange}></input>
                                 </div>
                             </div>
                             <div className="mb-3 row justify-content-center">
@@ -322,9 +488,7 @@ class Aluno extends Component {
                                 <label htmlFor="password" className="col-form-label required">Telefone</label>
                                 <input type="text" className="form-control" id="telefone" name="telefone" required onChange={this.handleChange}></input>
                                 </div>
-                            </div>
-
-                            
+                            </div>     
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={() => this.closeModal('Registration')}>
@@ -333,6 +497,90 @@ class Aluno extends Component {
                         <Button variant="success" type='submit'>
                             Criar
                         </Button>
+                    </Modal.Footer>
+                    </form>
+                </Modal>
+
+                <Modal show={this.state.showModalDeletion} onHide={() => this.closeModal('Deletion')} centered>
+                    <Modal.Header className='bg-dark text-white' closeButton>
+                    <Modal.Title>Confirmar Exclusão</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Tem certeza que deseja excluir o aluno {this.state.toDeleteItem && <span className='fw-bold'>{this.state.toDeleteItem.nome}</span>}?</Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={() => this.closeModal('Deletion')}>
+                        Cancelar
+                    </Button>
+                    <Button variant="primary" onClick={this.delete}>
+                        Confirmar
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Modal show={this.state.showModalView} onHide={() => this.closeModal('View')} centered>
+                    <Modal.Header className='bg-dark text-white' closeButton>
+                    <Modal.Title>Aluno {this.state.toViewItem && <>{this.state.toViewItem.titulo}</>}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                    {this.state.toViewItem && <>
+                            <div>
+                                <h5>Nome:</h5>
+                                <p>{this.state.toViewItem.nomeCompleto}</p>
+                            </div>
+                            <div>
+                                <h5>Matrícula:</h5>
+                                <p>{this.state.toViewItem.matricula}</p>
+                            </div>
+                            <div>
+                                <h5>Email:</h5>
+                                <p>{this.state.toViewItem.email}</p>
+                            </div>
+                            <div>
+                                <h5>Telefone:</h5>
+                                <p>{this.state.toViewItem.telefone}</p>
+                            </div>
+                            </>
+                    }
+                    </Modal.Body>
+                    <Modal.Footer>
+                    <Button variant="secondary" onClick={() => this.closeModal('View')}>
+                        Fechar
+                    </Button>
+                    </Modal.Footer>
+                </Modal>
+
+                <Modal show={this.state.showModalEdit} onHide={() => this.closeModal('Edit')} centered size='xl'>
+                    <Modal.Header className='bg-dark text-white' closeButton>
+                    <Modal.Title>Editar {this.state.toEditItem && <>{this.state.toEditItem.titulo}</>}</Modal.Title>
+                    </Modal.Header>
+                    <form onSubmit={this.registerForm}>
+                    <Modal.Body>
+                        {this.state.toEditItem && <>
+                            <div className="modal-body">
+                                <div className="container">
+                                        <div className="mb-3 row">
+                                            <div className="col-12">
+                                                <label htmlFor="inputName" className="col-4 col-form-label fw-bold required">Nome</label>
+                                                <input type="text" className="form-control" name="nomeCompleto" id="nomeCompleto" onChange={this.handleChange} value={this.state.nomeCompleto} required/>
+                                            </div>
+                                            <div className="col-12">
+                                                <label htmlFor="inputName" className="col-12 col-form-label fw-bold">Email</label>
+                                                <textarea rows="3" className='form-control' style={{resize: "none"}} name="email" onChange={this.handleChange} value={this.state.email}></textarea>
+                                            </div>
+                                            <div className="col-12">
+                                                <label htmlFor="inputName" className="col-12 col-form-label fw-bold">Telefone</label>
+                                                <textarea rows="3" className='form-control' style={{resize: "none"}} name="telefone" onChange={this.handleChange} value={this.state.telefone}></textarea>
+                                            </div>
+                                        </div>
+                                </div>
+                            </div>
+                        </>
+                        }
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={() => this.closeModal('Edit')}>
+                            Fechar
+                        </Button>
+                        <button type='submit' className="btn btn-primary">Salvar</button>
                     </Modal.Footer>
                     </form>
                 </Modal>
