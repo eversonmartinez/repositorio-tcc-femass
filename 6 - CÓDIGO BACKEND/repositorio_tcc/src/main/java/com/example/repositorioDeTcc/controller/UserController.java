36d011213108604b64e797096cff7bea2bf0d855
Everson Martinez <everson.mimartinez@gmail.com>
2024-11-10 21:28:06 -0300

    feat: backend operações de editar, findById e delete para User
    Feito de maneira simples, necessária revisão posterior.

--- 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/controller/UserController.java
+++ 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.repositorioDeTcc.controller;
 
-import com.example.repositorioDeTcc.dto.UserMinDTO;
+import com.example.repositorioDeTcc.dto.UserDTO;
+import com.example.repositorioDeTcc.dto.UserForListDTO;
 import com.example.repositorioDeTcc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,10 +19,31 @@ public class UserController {
     @Autowired
     UserService service;
 
-    @GetMapping
-    public ResponseEntity<List<UserMinDTO>> findAll(){
-        List<UserMinDTO> result = service.findAll();
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserForListDTO> findById(@PathVariable UUID id){
+        UserForListDTO result = service.findById(id);
         return ResponseEntity.ok().body(result);
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<UserForListDTO>> findAll(){
+        List<UserForListDTO> result = service.findAll();
+        return ResponseEntity.ok().body(result);
+    }
+
+    @PutMapping(value = "/{id}" , produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> update(@PathVariable UUID id, @RequestBody UserDTO user){
+
+        user = service.update(id, user);
+        return ResponseEntity.ok().body(user);
+
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id")UUID id){
+        service.delete(id);
+
+        return ResponseEntity.noContent().build();
     }
 
 }
--- 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/dto/UserForListDTO.java
+++ 6 - CÓDIGO BACKEND/repositorio_tcc/src/main/java/com/example/repositorioDeTcc/dto/UserForListDTO.java
@@ -1,7 +1,5 @@
 package com.example.repositorioDeTcc.dto;
 
-import com.example.repositorioDeTcc.model.Role;
-import com.example.repositorioDeTcc.model.TCC;
 import com.example.repositorioDeTcc.model.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,14 +10,14 @@ import java.util.UUID;
 @NoArgsConstructor
 @Getter
 @Setter
-public class UserMinDTO {
+public class UserForListDTO {
     private UUID id;
     private String nomeCompleto;
     private String role;
     private String matricula;
     private String email;
 
-    public UserMinDTO(User entity){
+    public UserForListDTO(User entity){
         this.id = entity.getId();
         this.nomeCompleto = entity.getNomeCompleto();
         this.role = entity.getRole().toString();
